5bd40b257cf001cb2ca05c5cb9a9386e5900c10a
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2026-06-18 07:44:43 +0900

    average

---
+++ 01jupyter/notebooks/source/01_intro/problems/00_average/average.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+
+int main() {
+  double a = 3.0;
+  double b = 4.0;
+  /* TODO: a と b の平均を計算して m に代入せよ (右辺を埋める). */
+  double m = ;
+  printf("average of %f and %f is %f\n", a, b, m);
+  return 0;
+}
---
+++ 01jupyter/notebooks/source/01_intro/problems/00_average/average.f90
@@ -0,0 +1,8 @@
+program average
+  real(8) :: a, b, m
+  a = 3.0d0
+  b = 4.0d0
+  ! TODO: a と b の平均を計算して m に代入せよ (右辺を埋める).
+  m =
+  print "(a,f0.6,a,f0.6,a,f0.6)", "average of ", a, " and ", b, " is ", m
+end program average
