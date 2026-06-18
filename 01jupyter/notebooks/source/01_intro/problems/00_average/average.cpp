0ae2bebf5c4ef2533f455b27fc38900c966c8cbf
Kenjiro Taura <tau@eidos.ic.i.u-tokyo.ac.jp>
2026-06-18 10:56:20 +0900

    average

--- 01jupyter/notebooks/source/01_intro/problems/00_average/average.cpp
+++ 01jupyter/notebooks/source/01_intro/problems/00_average/average.cpp
@@ -4,7 +4,7 @@ int main() {
   double a = 3.0;
   double b = 4.0;
   /* TODO: a と b の平均を計算して m に代入せよ (右辺を埋める). */
-  double m = ;
+  double m = a + b / 2;
   printf("average of %f and %f is %f\n", a, b, m);
   return 0;
 }
